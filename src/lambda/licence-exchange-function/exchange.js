d0061e3fe5aa31bacf0d4101ddb1e1e72e907369
areph <40balmung@gmail.com>
2022-08-20 20:36:09 +0900

    add fetch

--- src/lambda/licence-exchange-function/exchange.js
+++ src/lambda/licence-exchange-function/exchange.js
@@ -1,13 +1,15 @@
 const apiUrl = 'https://xrw58pwc75.execute-api.ap-northeast-1.amazonaws.com/dev/licence'
 const submitButton = document.getElementById("submit")
 submitButton.addEventListener('click', async () => {
-  const email = document.getElementById('email')
-  const trainingDate = document.getElementById('training-date')
+  const email = document.getElementById('email').value
+  const trainingDate = document.getElementById('training-date').value
+  const data = { "email": email, "training-date": trainingDate };
+  console.log(data);
   const response = await fetch(apiUrl, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ "email": email, "training-date": trainingDate })
+    body: JSON.stringify(data)
   })
-  const json = response.json()
+  const json = await response.json()
   console.log(json);
 })
