c37a8ebdacaa55ddfff01a87c5051372335b3aed
areph <40balmung@gmail.com>
2022-08-20 16:39:17 +0900

    Add todo list for index.js

--- src/lambda/licence-exchange-function/index.js
+++ src/lambda/licence-exchange-function/index.js
@@ -1,4 +1,13 @@
 exports.handler = async (event) => {
+
+  // TODO: パラメータとしてメールアドレスとトレーニング日を取得したい
+  // TODO: メールアドレスとトレーニング日をキーにしてDynamoDBからデータを取得したい
+  // TODO: 取得したライセンスコードをJSONとして返却したい
+
+  // エラーハンドリング
+  // TODO: パラメータにメールアドレスとトレーニング日がない場合はエラーとして処理を終了
+  // TODO: ライセンスコードが取得できない場合はエラーとして処理を終了
+
   console.log(event);
   const response = {
     statusCode: 200,
