111e874050a59163191b317df39db901b11176e8
areph <40balmung@gmail.com>
2022-08-20 16:45:41 +0900

    Add get event parameter

--- src/lambda/licence-exchange-function/index.js
+++ src/lambda/licence-exchange-function/index.js
@@ -1,6 +1,11 @@
 exports.handler = async (event) => {
 
+  console.log(event);
+
   // TODO: パラメータとしてメールアドレスとトレーニング日を取得したい
+  const email = event['email'];
+  const trainingDate = event['training-date'];
+
   // TODO: メールアドレスとトレーニング日をキーにしてDynamoDBからデータを取得したい
   // TODO: 取得したライセンスコードをJSONとして返却したい
 
@@ -8,7 +13,7 @@ exports.handler = async (event) => {
   // TODO: パラメータにメールアドレスとトレーニング日がない場合はエラーとして処理を終了
   // TODO: ライセンスコードが取得できない場合はエラーとして処理を終了
 
-  console.log(event);
+
   const response = {
     statusCode: 200,
     body: JSON.stringify('Hello from Lambda!'),
